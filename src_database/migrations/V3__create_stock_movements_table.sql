11bc036b79711943917934809544fb9fe755646e
erszemine <115109581+erszemine@users.noreply.github.com>
2025-05-26 19:22:31 +0000

    updated.

--- src_database/migrations/V3__create_stock_movements_table.sql
+++ src_database/migrations/V3__create_stock_movements_table.sql
@@ -3,11 +3,9 @@ CREATE TABLE IF NOT EXISTS StockMovement (
     product_id INTEGER NOT NULL,
     movement_type VARCHAR(20) NOT NULL, -- 'GIRIS', 'CIKIS', 'TRANSFER'
     amount INTEGER NOT NULL CHECK (amount > 0),
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    movement_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id INTEGER,
     explanation TEXT,
-    source_inventory_id INTEGER, -- Eğer birden fazla depo olacaksa
-    destination_inventory_id INTEGER,  -- Eğer birden fazla depo olacaksa
     FOREIGN KEY (product_id) REFERENCES Products(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE SET NULL,
 );
