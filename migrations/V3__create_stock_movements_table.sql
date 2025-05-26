0ffa8fb49b94a18efda4ee8b718c6ce8c32c5d5b
busra.ozkan <busra.ozkan529@gmail.com>
2025-05-26 10:53:53 +0000

    V3 and V4 files have started to be encoded.

---
+++ migrations/V3__create_stock_movements_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS StockMovement (
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER NOT NULL,
+    movement_type VARCHAR(20) NOT NULL, -- 'GIRIS', 'CIKIS', 'TRANSFER'
+    amount INTEGER NOT NULL CHECK (amount > 0),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER,
+    explanation TEXT,
+    source_inventory_id INTEGER, -- Eğer birden fazla depo olacaksa
+    destination_inventory_id INTEGER,  -- Eğer birden fazla depo olacaksa
+    FOREIGN KEY (product_id) REFERENCES Products(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE SET NULL
+);
+
